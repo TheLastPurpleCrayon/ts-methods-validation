56fe27c6a02dab960d1b4ac320cece4a78b6a692
Troy O <osbornt@carleton.edu>
2025-06-25 16:18:09 -0600

    bugfixing the printout

--- simulation.R
+++ simulation.R
@@ -1,6 +1,6 @@
 source("eacf_prelim.R")
 library(googlesheets4)
-library(hms)
+suppressMessages(library(hms))
 
 url <- "https://docs.google.com/spreadsheets/d/1zw2tG0Hya0vnF6qJFGar3dfsipFrWSgVL0_L2DeWjGY/edit?usp=sharing"
 sysname <- Sys.info()["nodename"] |> unname()
@@ -11,15 +11,17 @@ start_time.formatted <- format(start_time, "%Y-%m-%d %H:%M:%S")
 nseries <- as.numeric(readline("How many series?: "))
 Nsim <- as.numeric(readline("How many bootstrap loops?: "))
 
-start_printout <- paste0("EACF run\tTime:\t", start_time.formatted, 
-                         "\tSeries Length:\t", nseries,
-                         "\tNumber Bootstraps:\t", Nsim)
+start_printout <- c("EACF run", 
+                    "Time:", start_time.formatted, 
+                    "System:", sysname, 
+                    "Series Length:", nseries,
+                    "Number Bootstraps:", Nsim) |> matrix() |> t() |> as.data.frame()
 range_write(url, data = start_printout, range = "A1")
 #range_write(url, data = results, range = "B1")
 
 finish_time <- Sys.time()
 finish_time.formatted <- format(finish_time, "%Y-%m-%d %H:%M:%S")
 elapsed_time <- as_hms(finish_time - start_time) |> as.character() |> str_sub(1, 8)
-final_printout <- paste0("Finished at:\t", finish_time.formatted,
-                         "Time elapsed:\t", print(elapsed_time))
+final_printout <- c("Finished at:", finish_time.formatted,
+                    "Time elapsed:", print(elapsed_time)) |> matrix() |> t() |> as.data.frame()
 range_write(url, data = final_printout, range = "C1")
